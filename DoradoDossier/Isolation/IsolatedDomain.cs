10ccab56d4387173615faa3e4faae9b2a3ea1050
Chris Evans <chris.muttley.evans@gmail.com>
2015-10-22 15:04:11 +0100

    Removed Usings

--- DoradoDossier/Isolation/IsolatedDomain.cs
+++ DoradoDossier/Isolation/IsolatedDomain.cs
@@ -1,13 +1,7 @@
-﻿using RazorEngine.Templating;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Security;
 using System.Security.Permissions;
 using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Razor;
 
 namespace Dorado.Isolation
 {
